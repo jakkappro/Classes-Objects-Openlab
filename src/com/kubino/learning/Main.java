07dbe4e27db58ece1c43d9d99278e7a5a4de34c1
jakkappro <jakkappro@gmail.com>
2021-10-24 08:24:17 +0200

    2

--- src/com/kubino/learning/Main.java
+++ src/com/kubino/learning/Main.java
@@ -1,24 +1,23 @@
 package com.kubino.learning;
 
-import com.kubino.learning.Students.Student;
+import com.kubino.learning.Person.Person;
 
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student();
-        Student student1 = new Student();
+        Person[] people = {new Person("John", 21, 23000 )
+                ,new Person("Steve", 32, 40000 ), new Person("Martin", 16, 2700 )};
 
-        student.setName("John");
-        student.setStudentNumber(12345);
-        student.setAddress("ahoj");
-        student.setPhoneNumber(123456789);
 
-        student.setName("Sam");
-        student.setStudentNumber(54321);
-        student.setAddress("joha");
-        student.setPhoneNumber(987654321);
+        System.out.println(getPeopleBudget(people));
+    }
 
-        System.out.println(student.getStudentNumber() + ", " + student.getPhoneNumber() + ", " + student.getAddress());
+    private static int getPeopleBudget(Person... p) {
+        var r = 0;
 
-        System.out.println(student1.getStudentNumber() + ", " + student1.getPhoneNumber() + ", " + student1.getAddress());
+        for (var people : p) {
+            r += people.getBudget();
+        }
+
+        return r;
     }
 }
